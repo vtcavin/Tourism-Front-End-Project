68a73a69f39c0ad150639aeec44c5558ae25a332
vtcavin <141655732+vtcavin@users.noreply.github.com>
2024-04-15 18:11:21 +0530

    Add files via upload

---
+++ Tourism Front-End-Project/TourismWebsite.js
@@ -0,0 +1,26 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const bookingType = document.getElementById("booking-type");
+
+bookingType.addEventListener("click", (e) => {
+  Array.from(bookingType.getElementsByTagName("div")).forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  e.target.classList.add("active");
+});
+
